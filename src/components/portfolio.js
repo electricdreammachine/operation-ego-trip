24db1e885f46da5d2c35fa2031fb0d92be514f1c
ollie smith <omsmith89@gmail.com>
2018-07-24 21:44:07 +0100

    tidy up folder structure, set up intro gradient

---
+++ src/components/portfolio.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import styles from './portfolio.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.app}>
+        <header className={styles.appHeader}>
+          <img src={logo} className={styles.appLogo} alt="logo" />
+          <h1 className={styles.appTitle}>Welcome to React</h1>
+        </header>
+        <footer>
+          butts <br />
+          butts <br />
+          butts <br />
+          butts <br />
+          butts <br />
+          butts <br />
+          butts <br />
+          butts <br />
+          butts <br />
+          butts <br />
+          butts <br />
+          butts <br />
+          butts <br />
+          butts <br />
+          butts <br />
+          butts <br />
+          butts <br />
+          butts <br />
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './common/styles/index.css';
+import Portfolio from './components/portfolio';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Portfolio />, document.getElementById('root'));
