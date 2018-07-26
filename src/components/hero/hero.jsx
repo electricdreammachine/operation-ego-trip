27fff763c79cecb7ad18aa10ff7545b341bc32b6
ollie smith <omsmith89@gmail.com>
2018-07-26 21:42:36 +0100

    initial svg pattern, setting up of clipping mask

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { Fragment, Component } from 'react'
+import { ifElse, isNil, prop, pipe, always } from 'ramda'
 import Pattern from '../pattern'
 
-const Hero = () => (
-    <div>
-        <Pattern />
-    </div>
-)
+import styles from './hero.scss'
+
+class Hero extends Component {
+    constructor() {
+        super()
+        this.state = {
+            SVGNode: null,
+        }
+
+        this.getSVGNode = this.getSVGNode.bind(this)
+    }
+
+    getSVGNode(node) {
+        requestIdleCallback(() => this.setState({ SVGNode : node }))
+    }
+
+    render() {
+        const height = pipe (
+            prop(['SVGNode']),
+            ifElse(
+                isNil,
+                always(0),
+                (node) => node.getBoundingClientRect().height
+            )
+        )(this.state)
+
+        return (
+            <Fragment>
+                <svg className={styles.heroMask} ref={this.getSVGNode}>
+                    <polygon points={`0,0 0,${height} ${height},0`} style={{'fill':'lime', 'strokeWidth':'0'}} />
+                </svg>
+                <Pattern />
+            </Fragment>
+        )
+    }
+}
 
 export default Hero
