7cf60ef0b4be31615128c99f8ecc02939e5c5994
ollie smith <omsmith89@gmail.com>
2019-03-16 20:49:14 +0000

    fix art page render bug

--- src/pages/art.js
+++ src/pages/art.js
@@ -1,23 +1,36 @@
-import React, { useContext } from 'react'
+import React, { useContext, Fragment } from 'react'
 import 'common/styles/index.scss'
+import { isNil } from 'ramda'
 import { withStore, StoreContext } from 'store'
 import { Page } from 'components'
 import { Hero, Art as ArtSection, Contact } from 'sections'
 
 const Art = () => {
-  const {
-    domain: {
-      intro: { edges: [{ node: introduction }] },
-      art: { pieces },
-      contact: { edges: [{node: contactInfo}] },
-    }
-  } = useContext(StoreContext)
+  const availableData = useContext(StoreContext)
+
+  let PopulatedSections = null
+
+  if (!isNil(availableData)) {
+    const {
+      domain: {
+        intro: { edges: [{ node: introduction }] },
+        art: { pieces },
+        contact: { edges: [{node: contactInfo}] },
+      }
+    } = useContext(StoreContext)
+
+    PopulatedSections = (
+      <Fragment>
+        <Hero elementType="header" introduction={introduction} />
+        <ArtSection art={pieces}/>
+        <Contact name="Contact" contactInfo={contactInfo} />
+      </Fragment>
+    )
+  }
 
   return (
     <Page>
-      <Hero elementType="header" introduction={introduction} />
-      <ArtSection art={pieces}/>
-      <Contact name="Contact" contactInfo={contactInfo} />
+      {PopulatedSections}
     </Page>
   )
 }
