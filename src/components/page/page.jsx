bc8abb285e4f19b7d92aa818c2637c41a2c21cd7
ollie smith <omsmith89@gmail.com>
2019-05-01 09:41:16 +0100

    add og meta tags via helmet

--- src/components/page/page.jsx
+++ src/components/page/page.jsx
@@ -2,26 +2,42 @@ import React, { useContext, Fragment } from 'react'
 import { CSSTransition } from 'react-transition-group'
 import { isNil, propOr } from 'ramda'
 import { StoreContext } from 'store'
+import Helmet from 'react-helmet'
+import ogImage from 'common/assets/images/share-img.png'
 import styles from './page.module.scss'
 
 const Page = ({ children }) => {
   const { state: { patternRef } } = useContext(StoreContext)
 
   return (
-    <div className={styles.page}>
-      <CSSTransition
-        in={!isNil(propOr(null, 'current', patternRef))}
-        classNames={{
-          enter: styles.enter,
-          enterActive: styles.enterActive,
-        }}
-        timeout={1000}
-      >
-        <Fragment>
-          {children}
-        </Fragment>
-      </CSSTransition>
-    </div>
+    <Fragment>
+      <Helmet
+        meta={[
+          {
+            property: `og:title`,
+            content: 'olliesmith.dev',
+          },
+          {
+            property: `og:image`,
+            content: ogImage,
+          }
+        ]}
+      />
+      <div className={styles.page}>
+        <CSSTransition
+          in={!isNil(propOr(null, 'current', patternRef))}
+          classNames={{
+            enter: styles.enter,
+            enterActive: styles.enterActive,
+          }}
+          timeout={1000}
+        >
+          <Fragment>
+            {children}
+          </Fragment>
+        </CSSTransition>
+      </div>
+    </Fragment>
   )
 }
 
