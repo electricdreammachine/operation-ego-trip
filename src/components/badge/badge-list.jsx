566e05890450c345d7d9416920c978a484494a1e
Ollie <hello@olliesmith.dev>
2020-04-15 06:58:38 +0100

    fix relative import for build

--- src/components/badge/badge-list.jsx
+++ src/components/badge/badge-list.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { map } from 'ramda'
 import styles from './badge.module.scss'
 
-import Badge from '../Badge'
+import Badge from './Badge'
 
 const BadgeList = ({ listItems }) =>
   map(item => <Badge className={styles.badgeListItem}>{item}</Badge>, listItems)
